9b440d715e7802b92d7d3032b9d81a50ecef6c9d
Jeremy Evans <code@jeremyevans.net>
2016-08-29 12:30:21 -0700

    Make roda-hash builder a little more realistic
    This is a little bit slower, but a more realistic comparison
    of how something would actually be done in real life.  Also,
    it doesn't return the same results for POST as for GET.

--- builders/roda-hash.rb
+++ builders/roda-hash.rb
@@ -2,7 +2,7 @@ roda_hash_routes = lambda do |f, level, prefix|
   base = BASE_ROUTE.dup
   ROUTES_PER_LEVEL.times do
     if level == 1
-      f.puts "  routes['#{prefix}#{base}'] = lambda do |r|"
+      f.puts "  get '#{prefix}#{base}' do |r|"
       f.puts "    '#{RESULT.call(prefix[1..-1] + base)}'"
       f.puts "  end"
     else
@@ -15,8 +15,17 @@ end
 File.open("#{File.dirname(__FILE__)}/../apps/roda-hash_#{LEVELS}_#{ROUTES_PER_LEVEL}.rb", 'wb') do |f|
   f.puts "require 'roda'"
   f.puts "class App < Roda"
-  f.puts "routes = {}"
+  f.puts "  @routes = {}"
+  f.puts "  @routes['GET'] = {}"
+  f.puts "  class << self; attr_reader :routes; end"
+  f.puts "  def self.get(route, &block)"
+  f.puts "    @routes['GET'][route] = block"
+  f.puts "  end"
   roda_hash_routes.call(f, LEVELS, '/')
-  f.puts "route{|r| instance_exec(r, &routes[r.remaining_path])}"
+  f.puts "  route do |r|"
+  f.puts "    if (h = self.class.routes[r.request_method]) && (b = h[r.remaining_path])"
+  f.puts "      instance_exec(r, &b)"
+  f.puts "    end"
+  f.puts "  end"
   f.puts "end"
 end
