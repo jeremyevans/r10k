860d188448e42eb594877085667bf9f892a5cf1d
Jeremy Evans <code@jeremyevans.net>
2016-08-29 14:59:47 -0700

    Switch roda-hash builder to roda-static-routing
    After seeing how good performance was with the roda-hash builder,
    I decided to app a plugin to Roda that used a similar approach,
    so switch r10k to use that plugin.

---
+++ builders/roda-static-routing.rb
@@ -0,0 +1,23 @@
+roda_static_routes = lambda do |f, level, prefix|
+  base = BASE_ROUTE.dup
+  ROUTES_PER_LEVEL.times do
+    if level == 1
+      f.puts "  static_get '#{prefix}#{base}' do |r|"
+      f.puts "    '#{RESULT.call(prefix[1..-1] + base)}'"
+      f.puts "  end"
+    else
+      roda_static_routes.call(f, level-1, "#{prefix}#{base}/")
+    end
+    base.succ!
+  end
+end
+
+File.open("#{File.dirname(__FILE__)}/../apps/roda-static-routing_#{LEVELS}_#{ROUTES_PER_LEVEL}.rb", 'wb') do |f|
+  f.puts "require 'roda'"
+  f.puts "class App < Roda"
+  f.puts "  plugin :static_routing"
+  roda_static_routes.call(f, LEVELS, '/')
+  f.puts "  route do |r|"
+  f.puts "  end"
+  f.puts "end"
+end
