9fa71a19b883612e6abdee03a17028e1f9fb34d1
Jeremy Evans <code@jeremyevans.net>
2021-07-09 15:27:56 -0700

    Switch Roda builder to use a single matcher per level
    This is to benefit from the optimization in Roda 3.46.0+.
    
    Drop the direct_call plugin loading as it is not needed.

--- builders/roda.rb
+++ builders/roda.rb
@@ -1,16 +1,18 @@
 roda_routes = lambda do |f, level, prefixes, lvars|
   base = BASE_ROUTE.dup
-  spaces = "  " * (LEVELS - level + 1)
-  meth = (level == 1 ? 'is' : 'on')
+  spaces = "  " * (2*(LEVELS - level + 1) - 1)
   prefix = prefixes.join('/')
+  meth = (level == 1 ? 'get' : 'on')
   ROUTES_PER_LEVEL.times do
-    f.puts "#{spaces}r.#{meth} '#{base}', String do |#{lvars.last}|"
+    f.puts "#{spaces}r.on '#{base}' do"
+    f.puts "#{spaces}  r.#{meth} String do |#{lvars.last}|"
     if level == 1
-      f.puts "#{spaces}  \"#{RESULT.call(prefixes.empty? ? base : "#{prefix}/#{base}")}#{lvars.map{|lvar| "-\#{#{lvar}}"}.join}\""
+      f.puts "#{spaces}    \"#{RESULT.call(prefixes.empty? ? base : "#{prefix}/#{base}")}#{lvars.map{|lvar| "-\#{#{lvar}}"}.join}\""
     else
       roda_routes.call(f, level-1, prefixes + [base.dup], lvars + [lvars.last.succ])
     end
     base.succ!
+    f.puts "#{spaces}  end"
     f.puts "#{spaces}end"
   end
 end
@@ -18,7 +20,6 @@ end
 File.open("#{File.dirname(__FILE__)}/../apps/roda_#{LEVELS}_#{ROUTES_PER_LEVEL}.rb", 'wb') do |f|
   f.puts "# frozen-string-literal: true"
   f.puts "require 'roda'"
-  f.puts "Roda.plugin :direct_call"
   f.puts "Roda.route do |r|"
   f.puts "r.get do"
   roda_routes.call(f, LEVELS, [], ['a'])
