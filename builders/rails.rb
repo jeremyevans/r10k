da10e926e159a834039d9563fa5a42d6b47e4d36
Hartley McGuire <skipkayhil@gmail.com>
2025-02-15 11:33:41 -0500

    Use latest `config.load_defaults` in Rails Apps
    Most applications should be using the more modern defaults, so this
    makes the benchmark more realistic.
    
    Note: this sets `config.yjit = false` because Rails will enable YJIT
    automatically after the application is initialized (so that boot code is
    never compiled). Instead, the benchmark harness can choose whether to
    enable YJIT for more "fair" comparisons.

--- builders/rails.rb
+++ builders/rails.rb
@@ -33,6 +33,8 @@ File.open("#{File.dirname(__FILE__)}/../apps/rails_#{LEVELS}_#{ROUTES_PER_LEVEL}
 # frozen-string-literal: true
 require 'action_controller/railtie'
 class AppClass < Rails::Application
+  config.load_defaults Rails::VERSION::STRING.to_f
+  config.yjit = false
   config.secret_key_base = 'foo'
   config.cache_classes = true
   config.eager_load = true
